5aff40ba13f2f52db7535890e6f26927818c0f10
gpm <gpm80@mail.ru>
2020-11-15 02:28:31 +0300

    Мерж веток разработки.

--- src/main/java/ru/lod/spbalert/repository/GroupAlertRepository.java
+++ src/main/java/ru/lod/spbalert/repository/GroupAlertRepository.java
@@ -6,6 +6,9 @@ import java.util.stream.Stream;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import ru.lod.spbalert.dao.GroupAlertDocument;
 
+/**
+ * репозиторий для работы с сгруппированными данными.
+ */
 public interface GroupAlertRepository extends ElasticsearchRepository<GroupAlertDocument, String> {
 
     Stream<GroupAlertDocument> findByTimePointBetween(Date start, Date end);
